d67449f05a5cc64d78b0f1db6f2d462e9c7d552e
Shibo Chen <shibochenny@gmail.com>
2024-07-15 00:52:33 -0400

    ready to deploy

---
+++ frontend/src/components/svgs/X.jsx
@@ -0,0 +1,6 @@
+const XSvg = (props) => (
+  <svg aria-hidden="true" viewBox="0 0 24 24" {...props}>
+    <path d="M12 2.635l2.292 7.073h7.427l-6.018 4.37 2.292 7.073-6.018-4.37-6.018 4.37 2.292-7.073-6.018-4.37h7.427z" />
+  </svg>
+);
+export default XSvg;
